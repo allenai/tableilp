5bb13e6a1b2d0b94868203803a0351593cd7711a
Ashish Sabharwal <AshishS@allenai.org>
2015-09-17 12:49:01 -0700

    update TableParams

--- src/main/scala/org/allenai/ari/solvers/tableilp/params/TableParams.scala
+++ src/main/scala/org/allenai/ari/solvers/tableilp/params/TableParams.scala
@@ -6,12 +6,13 @@ import com.google.inject.name.Named
 /** Various parameters related to knowledge tables.
   *
   * @param folder Name of the folder from which to read tables
-  * @param ignoreListStr A comma-separated list of able IDs to ignore
+  * @param ignoreListStr A comma-separated list of table IDs to ignore
   * @param maxTablesPerQuestion Max number of tables to consider per question
-  * @param questionToTablesCache Name of the cheat sheet mapping question to relevant tables
+  * @param questionToTablesCache Name of a debugging cheat sheet mapping question to relevant tables
   * @param useCachedTablesForQuestion Whether to use the above cheat sheet
   * @param rankThreshold Table rank must be at most this much for selection, if useThreshold = true
   * @param useRankThreshold Whether to use the above threshold on rank
+  * @param allowedTitleAlignmentsFile A CSV file specifying which columns in two tables may align
   */
 class TableParams @Inject() (
     @Named("tables.folder") val folder: String,
@@ -20,12 +21,13 @@ class TableParams @Inject() (
     @Named("tables.questionToTablesCache") val questionToTablesCache: String,
     @Named("tables.useCachedTablesForQuestion") val useCachedTablesForQuestion: Boolean,
     @Named("tables.rankThreshold") val rankThreshold: Double,
-    @Named("tables.useRankThreshold") val useRankThreshold: Boolean
+    @Named("tables.useRankThreshold") val useRankThreshold: Boolean,
+    @Named("tables.allowedTitleAlignmentsFile") val allowedTitleAlignmentsFile: String
 ) {
   val ignoreList: Seq[Int] = if (ignoreListStr == "") {
     Seq.empty
   } else {
-    ignoreListStr.split(',').map(_.toInt).toSeq
+    ignoreListStr.split(',').map(_.toInt)
   }
 }
 
@@ -38,6 +40,7 @@ object TableParams {
     questionToTablesCache = "",
     useCachedTablesForQuestion = false,
     rankThreshold = 0.25d,
-    useRankThreshold = false
+    useRankThreshold = false,
+    allowedTitleAlignmentsFile = ""
   )
 }
