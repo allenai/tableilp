ed19ed738bf5a6c081bc71fcba5f26cf581dbbdb
Ashish Sabharwal <AshishS@allenai.org>
2015-09-21 11:01:19 -0700

    change parameter name

--- src/main/scala/org/allenai/ari/solvers/tableilp/params/IlpWeights.scala
+++ src/main/scala/org/allenai/ari/solvers/tableilp/params/IlpWeights.scala
@@ -10,13 +10,13 @@ import com.google.inject.name.Named
   * @param minTitleQConsAlignment Minimum threshold for considering title-to-question alignment
   * @param minTitleTitleAlignment Minimum threshold for considering title-to-title alignment
   * @param activeCellObjCoeff How much does an active cell contribute to the objective function
-  * @param activeRowObjCoeff How much does an active row contribute to the objective function
   * @param activeColObjCoeff How much does an active column contribute to the objective function
   * @param activeTitleObjCoeff How much does an active title contribute to the objective function
   * @param tableScoreObjCoeff How much does the table matching score contribute to the obj function
   * @param activeQConsObjCoeff How much does an active qcons contribute to the objective function
   * @param activeChoiceObjCoeff How much does an active choice contribute to the objective function
   * @param tableUsagePenalty How much to penalize the use of a table
+  * @param rowUsagePenalty How much to penalize the use of a row
   * @param interTableAlignmentPenalty How much to penalize alignments across two tables
   * @param maxAlignmentsPerQCons How many external alignments may a question constituent have
   */
@@ -28,13 +28,13 @@ class IlpWeights @Inject() (
   @Named("weights.minTitleQChoiceAlignment") val minTitleQChoiceAlignment: Double,
   @Named("weights.minTitleTitleAlignment") val minTitleTitleAlignment: Double,
   @Named("weights.activeCellObjCoeff") val activeCellObjCoeff: Double,
-  @Named("weights.activeRowObjCoeff") val activeRowObjCoeff: Double,
   @Named("weights.activeColObjCoeff") val activeColObjCoeff: Double,
   @Named("weights.activeTitleObjCoeff") val activeTitleObjCoeff: Double,
   @Named("weights.tableScoreObjCoeff") val tableScoreObjCoeff: Double,
   @Named("weights.activeQConsObjCoeff") val activeQConsObjCoeff: Double,
   @Named("weights.activeChoiceObjCoeff") val activeChoiceObjCoeff: Double,
   @Named("weights.tableUsagePenalty") val tableUsagePenalty: Double,
+  @Named("weights.rowUsagePenalty") val rowUsagePenalty: Double,
   @Named("weights.interTableAlignmentPenalty") val interTableAlignmentPenalty: Double,
   @Named("weights.maxAlignmentsPerQCons") val maxAlignmentsPerQCons: Int
 ) {}
@@ -49,13 +49,13 @@ object IlpWeights {
     minTitleQChoiceAlignment = 0.2d,
     minTitleTitleAlignment = 0.2d,
     activeCellObjCoeff = 0d,
-    activeRowObjCoeff = 0d,
     activeColObjCoeff = 0d,
     activeTitleObjCoeff = 0d,
     tableScoreObjCoeff = 1d,
     activeQConsObjCoeff = 1d,
     activeChoiceObjCoeff = 100d,
     tableUsagePenalty = 4d,
+    rowUsagePenalty = 1d,
     interTableAlignmentPenalty = 1d,
     maxAlignmentsPerQCons = 2
   )
