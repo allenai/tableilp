54a2c745ec2f6c561c26a8b096bb9f5f1af49eac
Ashish Sabharwal <AshishS@allenai.org>
2015-09-23 16:16:33 -0700

    add utility methods to manipulate Resource files

--- src/main/scala/org/allenai/ari/solvers/tableilp/Utils.scala
+++ src/main/scala/org/allenai/ari/solvers/tableilp/Utils.scala
@@ -1,7 +1,13 @@
 package org.allenai.ari.solvers.tableilp
 
+import org.allenai.common.{ Logging, Resource }
+
+import java.io.{ InputStreamReader, InputStream }
+
+import scala.io.{ BufferedSource, Source }
+
 /** General utilities */
-object Utils {
+object Utils extends Logging {
   /** A common use case for groupBy. Takes in a sequence of pairs, groups them by the first
     * element, and returns a map from the group identifier to a sequence of second elements
     * of the matching pairs. E.g., ((a,1), (b,2), (a,3), (b,4)) turns into {a -> (1,3),
@@ -13,4 +19,29 @@ object Utils {
 
   /** A small value to handle floating point mismatches. */
   val eps = 1e-6
+
+  /** Get a resource file as a Stream */
+  def getResourceAsStream(name: String): InputStream = {
+    getClass.getClassLoader.getResourceAsStream(name)
+  }
+
+  /** Get a resource file as a Reader */
+  def getResourceAsReader(name: String): InputStreamReader = {
+    new InputStreamReader(getResourceAsStream(name))
+  }
+
+  /** Get a resource file as a buffered Source */
+  def getResourceAsSource(name: String): BufferedSource = {
+    Source.fromInputStream(getResourceAsStream(name))
+  }
+
+  /** Get a resource file as a sequence of lines */
+  def getResourceAsLines(name: String): Seq[String] = {
+    logger.info(s"Loading $name")
+    Resource.using(getResourceAsSource(name)) { input =>
+      val lines = input.getLines().toVector // convert to vector to force stream to be processed
+      logger.trace(s"lines:\n\t${lines.mkString("\n\t")}")
+      lines
+    }
+  }
 }
