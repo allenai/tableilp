121469abb85d88582b515a167fecd02846f349eb
Ashish Sabharwal <AshishS@allenai.org>
2015-09-24 16:35:01 -0700

    add comment explaining the need for PWD

--- src/main/bin/tableilp-solver.sh
+++ src/main/bin/tableilp-solver.sh
@@ -7,10 +7,9 @@ CLASS_NAME="org.allenai.ari.solvers.tableilp.TableIlpServer"
 SCRIPT_DIR=`dirname $0`
 SHORT_NAME=`basename $0 .sh`
 
-# dynamic libraries for the SCIP ILP engine, on unix and osx
+# absolute paths for dynamic libraries for the SCIP ILP engine, for unix and osx
 SCIP_LIBDIR="$PWD/$SCRIPT_DIR/../lib"
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCIP_LIBDIR"
-DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$SCIP_LIBDIR"
-export LD_LIBRARY_PATH DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCIP_LIBDIR"
+export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$SCIP_LIBDIR"
 
 . "${SCRIPT_DIR}/run-class.sh" "$CLASS_NAME" "$SHORT_NAME" "$@"
