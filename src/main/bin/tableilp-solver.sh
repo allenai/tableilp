d73b0bdb81f0d07e339d15e9d4d2b41187956d57
Ashish Sabharwal <AshishS@allenai.org>
2015-09-24 15:42:37 -0700

    add LD_LIBRARY_PATH for running staged TableILP solver

--- src/main/bin/tableilp-solver.sh
+++ src/main/bin/tableilp-solver.sh
@@ -6,4 +6,11 @@ CLASS_NAME="org.allenai.ari.solvers.tableilp.TableIlpServer"
 
 SCRIPT_DIR=`dirname $0`
 SHORT_NAME=`basename $0 .sh`
+
+# dynamic libraries for the SCIP ILP engine, on unix and osx
+SCIP_LIBDIR="$PWD/$SCRIPT_DIR/../lib"
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCIP_LIBDIR"
+DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$SCIP_LIBDIR"
+export LD_LIBRARY_PATH DYLD_LIBRARY_PATH
+
 . "${SCRIPT_DIR}/run-class.sh" "$CLASS_NAME" "$SHORT_NAME" "$@"
