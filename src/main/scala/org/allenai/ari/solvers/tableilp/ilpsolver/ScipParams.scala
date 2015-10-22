537775228cf61218396ae5725c3d5cb77cae18f4
Ashish Sabharwal <AshishS@allenai.org>
2015-10-22 15:49:36 -0700

    fix incorrect comment

--- src/main/scala/org/allenai/ari/solvers/tableilp/ilpsolver/ScipParams.scala
+++ src/main/scala/org/allenai/ari/solvers/tableilp/ilpsolver/ScipParams.scala
@@ -5,10 +5,10 @@ import com.google.inject.name.Named
 
 /** Various parameters for the SCIP ILP solver
   *
-  * @param timeLimit Overall time limit for SCIP in seconds once it starts solving the model
-  * @param logFile Log file where SCIP output is stored for debugging purposes
-  * @param messagehdlrQuiet Whether to have SCIP's message handler be quiet or write to log file
-  * @param printVersion Integer, indicating whether to print SCIP's version to log
+  * @param timeLimit overall time limit for SCIP in seconds once it starts solving the model
+  * @param logFile log file where SCIP output is stored for debugging purposes
+  * @param messagehdlrQuiet whether to have SCIP's message handler be quiet or write to stdout
+  * @param printVersion integer, indicating whether to print SCIP's version to log
   */
 class ScipParams @Inject() (
   @Named("scip.timeLimit") val timeLimit: Double,
