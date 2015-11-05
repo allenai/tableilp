40c2731fcf38980f6f5276d6a9938069dd7a37c6
sbhaktha <sumithrab@allenai.org>
2015-11-05 10:42:22 -0800

    Fix TableIlp Startup script

--- src/main/bin/tableilp-solver.sh
+++ src/main/bin/tableilp-solver.sh
@@ -12,7 +12,7 @@ SHORT_NAME=`basename $0 .sh`
 
 # absolute paths for dynamic libraries for the SCIP ILP engine, for unix and osx
 SCIP_LIBDIR="$PWD/$SCRIPT_DIR/../lib"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCIP_LIBDIR"
-export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$SCIP_LIBDIR"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCIP_LIBDIR
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$SCIP_LIBDIR
 
 . "${SCRIPT_DIR}/run-class.sh" "$CLASS_NAME" "$SHORT_NAME" "$@"
