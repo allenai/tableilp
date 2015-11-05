b0dc51c3fa4cf2458fbc972766b555552f78b1ea
sbhaktha <sumithrab@allenai.org>
2015-11-05 13:44:26 -0800

    Jesse's idea for path + put back quotes :-)

--- src/main/bin/tableilp-solver.sh
+++ src/main/bin/tableilp-solver.sh
@@ -11,8 +11,8 @@ SCRIPT_DIR=`dirname $0`
 SHORT_NAME=`basename $0 .sh`
 
 # absolute paths for dynamic libraries for the SCIP ILP engine, for unix and osx
-SCIP_LIBDIR="$PWD/$SCRIPT_DIR/../lib"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCIP_LIBDIR
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$SCIP_LIBDIR
+SCIP_LIBDIR=`cd "$SCRIPT_DIR/../lib"; pwd`
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCIP_LIBDIR"
+export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$SCIP_LIBDIR"
 
 . "${SCRIPT_DIR}/run-class.sh" "$CLASS_NAME" "$SHORT_NAME" "$@"
