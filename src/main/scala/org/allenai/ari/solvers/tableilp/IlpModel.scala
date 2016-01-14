1cae3cc59ed83a227b3b4ee0d6eef0c6b7b0d30f
Ashish Sabharwal <AshishS@allenai.org>
2016-01-14 11:18:56 -0800

    minor edit to relation variable name

--- src/main/scala/org/allenai/ari/solvers/tableilp/IlpModel.scala
+++ src/main/scala/org/allenai/ari/solvers/tableilp/IlpModel.scala
@@ -1350,8 +1350,9 @@ class IlpModel(
     flipped: Boolean): RelationMatchVariable = {
     val matchStartStr = if (matchStart >= 0) matchStart.toString else "None"
     val matchEndStr = if (matchEnd >= 0) matchEnd.toString else "None"
+    val choiceIdxStr = if (choiceIdx.isDefined) choiceIdx.get.toString else "None"
     val name = s"rel_t${tableIdx}_c${col1Idx}_c${col2Idx}_m${matchStartStr}_m${matchEndStr}_" +
-      s"ch$choiceIdx"
+      s"ch$choiceIdxStr"
     val variable = ilpSolver.createBinaryVar(name, coeff)
     ilpSolver.addVar(variable)
     RelationMatchVariable(tableIdx, col1Idx, col2Idx, matchStart, matchEnd, choiceIdx,
