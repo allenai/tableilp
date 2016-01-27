d200f9a9a470ff1f273957bd54040a60047a5305
Ashish Sabharwal <AshishS@allenai.org>
2016-01-26 23:46:11 -0800

    more overrides

--- src/main/scala/org/allenai/ari/solvers/tableilp/AlignmentFunction.scala
+++ src/main/scala/org/allenai/ari/solvers/tableilp/AlignmentFunction.scala
@@ -154,7 +154,7 @@ private class EntailmentSimilarity(
   // set of words that should be ignored for entailment calculation if they are the hypothesis;
   // note that in WordNet, consumer -> person -> causal_agent -> cause !
   // Additional candidates: matter, substance, whole, part, cause, unit, event, relation
-  private val ignoreHypothesisSet = Set("object", "measure")
+  private val ignoreHypothesisSet = Set("object", "measure", "part")
   private def getEntailmentScore(text1: String, text2: String): Double = {
     val key = text1 + "----" + text2
     // If Redis cache is being used and contains 'key', return the stored value; otherwise
